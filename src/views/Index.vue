65e13ce7ee863c84fc0344319186c774362f054e
KabakiAntony <kabaki.antony@gmail.com>
2022-05-20 12:43:12 +0300

    created the index view and imported various components to be used in it

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <LandingPage msg="Welcome, login to use the system."/>
+</template>
+
+<script>
+import LandingPage from "../components/LandingPage.vue";
+
+export default{
+    name: "Index",
+    components:{
+        LandingPage
+    }
+}
+</script>
+
+<style>
+</style>
