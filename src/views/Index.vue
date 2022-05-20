a368e3307397ed1eb65cb36afb64b439d00d6b29
KabakiAntony <kabaki.antony@gmail.com>
2022-05-20 12:50:01 +0300

    refined the index view page instead of using multiple components

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,46 @@
 <template>
-    <LandingPage msg="Welcome, login to use the system."/>
+    <!-- <Spinner /> -->
+     <!-- <Transition name="toast">
+       <ShowAlert  v-if='show' :class='type' :message='message'/>
+    </Transition> -->
+    <div class="holder">
+        <h1 class="index-h1">Welcome, login to use the system.</h1>
+        <img
+            alt="landing page image"
+            class="side-img"
+            src="../assets/cute_eve_login.svg"
+        />
+    <CommonForm  v-bind="commonFormProps" @on_submit="handleSubmit" :action="action" />
+    </div>
 </template>
 
 <script>
-import LandingPage from "../components/LandingPage.vue";
+import CommonForm from "@/components/CommonForm.vue"
+// import ShowAlert from "@/components/ShowAlert.vue"
+// import Spinner from '@/components/Spinner.vue'
+// import { unloadToast, loadToast, loadSpinner, unloadSpinner } from "../utils"
 
 export default{
     name: "Index",
     components:{
-        LandingPage
-    }
+        CommonForm
+    },
+    data(){
+      return {
+        commonFormProps:{
+        header:"Login",
+        submit_text:"Login",
+        bottom_text:"Forgot Password ?",
+        bottom_link_text:"Send link",
+        route_name:"Forgot"
+      },
+      type:null,
+      message:null,
+      show:false,
+      action:null,
+      }
+
+  },
 }
 </script>
 
