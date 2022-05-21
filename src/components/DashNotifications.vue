3d4b18c1c01332da06338c0752a95c6b1d6eead1
KabakiAntony <kabaki.antony@gmail.com>
2022-05-21 12:05:30 +0300

    created a dashboard notification component

---
+++ src/components/DashNotifications.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="type" id="dashboard_notification">{{message}}</div>
+</template>
+
+<script>
+export default {
+    name:"DashNotifications",
+    props:['type', 'message']
+}
+</script>
+
+<style>
+.error{
+  background-color: #e97971;
+  border: 2px solid rgb(231, 24, 9);
+}
+.success {
+  background-color: #d4edda;
+  border: 2px solid rgb(44, 148, 2);
+}
+.none{
+    display:none;
+}
+#dashboard_notification{
+  margin: 20px auto;
+  color:black;
+  text-align: left;
+  border-radius: 0.5rem;
+  padding: 0.6rem;
+  font-size:100%;
+}
+</style>
