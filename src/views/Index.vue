823702feb0a21564cabc3441a0f080335d5efb9c
KabakiAntony <kabaki.antony@gmail.com>
2022-05-24 15:01:25 +0300

    created a handlesubmit function that does the logging into the system

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -24,7 +24,7 @@
 import CommonForm from "@/components/CommonForm.vue"
 import ShowAlert from "@/components/ShowAlert.vue"
 import Spinner from '@/components/Spinner.vue'
-// import { unloadToast, loadToast, loadSpinner, unloadSpinner } from "../utils"
+import { unloadToast, loadToast, loadSpinner, unloadSpinner } from "../utils"
 
 export default{
     name: "Index",
@@ -48,6 +48,33 @@ export default{
       action:null,
       disabled:false,
       }
+
+  },
+  methods:{
+    loadSpinner,
+    unloadSpinner, 
+    unloadToast, 
+    loadToast,
+    async handleSubmit(theForm){
+            this.action="submitting"
+            this.commonFormProps.submit_text =" "
+            this.loadSpinner()
+              let data = await this.$store.dispatch('logIn',theForm)
+              if(data.status === 200){
+                this.unloadSpinner()
+                this.loadToast(data.data.message, "success")
+                this.unloadToast()
+                setTimeout(()=>{
+                this.$router.push({name: 'Dashboard'})
+                },3000)
+            } else {
+              this.unloadSpinner()
+              this.loadToast(data, "error")
+              this.unloadToast()
+              }
+            this.action = ""
+            this.commonFormProps.submit_text = "Login"
+        },
 
   },
 }
