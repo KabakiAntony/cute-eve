03ee8354f7c55986e19e38c24830cccbd450bbdc
KabakiAntony <kabaki.antony@gmail.com>
2022-05-24 10:16:52 +0300

    initialized logged in state for the navigation using store states

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup>
-import { RouterView, RouterLink } from "vue-router";
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  methods:{
+    async logOut(){
+      try{
+          this.$store.commit('SET_IS_LOGGED_IN',false)
+          this.$store.commit('SET_AUTH_TOKEN',null)
+          this.$store.commit('SET_SCREEN_NAME',null)
+          this.$router.push({name:"SignIn"})
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+  },
+  computed:{
+    ...mapGetters({
+      isLoggedIn:"IsLoggedIn", 
+      screen_name:"ScreenName",
+      }),
+  },  
+}
 </script>
 
 <template>
@@ -7,19 +29,17 @@ import { RouterView, RouterLink } from "vue-router";
     <ul>
       <li class="logo">Cute Eve</li>
       <div class="right-nav">
-        <!-- <li v-if="isLoggedIn">
-          <RouterLink>Hello</RouterLink>
-        <a class="Signout">Logout</a>
+        <li v-if="isLoggedIn">
+          <router-link :to="{ name: 'Dashboard'}">Hello {{ screen_name }}</router-link>
+          <a class="Signout" @click="logOut">Logout</a>
         </li>
         <li v-else>
-          <RouterLink>Home</RouterLink>
-          <RouterLink>Home</RouterLink>
-          <RouterLink>Home</RouterLink>
-        </li> -->
+          <router-link :to="{ name: 'Index' }">Home</router-link>
+        </li>
       </div>
     </ul>
   </nav>
-  <RouterView />
+  <router-view/>
   <footer>
     <p>&copy; Cute Eve Limited  2022&nbsp;&nbsp;<a href="https://twitter.com/kabakiantony">Developed by</a></p>
   </footer>
