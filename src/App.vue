d19d079a45018bf1947b9c9c7bca0935ffc32bbd
KabakiAntony <kabaki.antony@gmail.com>
2022-06-19 18:45:53 +0200

    added header section for mobile menu

--- src/App.vue
+++ src/App.vue
@@ -29,17 +29,26 @@ export default {
 </script>
 
 <template>
-  <nav>
-    <ul>
-      <li class="logo">Cute Eve</li>
-      <div class="right-nav">
-        <li v-if="isLoggedIn">
-          <router-link :to="{ name: 'Dashboard'}">Hi {{ screenName }}</router-link>
-          <a class="logout" @click="logOut">Logout</a>
-        </li>
-      </div>
-    </ul>
-  </nav>
+<header>
+   <!-- Logo -->
+        <li class="logo">Cute Eve</li>
+        <!-- Hamburger icon -->
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+        <!-- Menu -->
+        <nav>
+            <ul class="menu" v-if="isLoggedIn">
+              <!-- v-if="isLoggedIn" -->
+                <li>
+                <router-link :to="{ name: 'Dashboard'}">Hi {{ screenName }}</router-link>
+                </li>
+                <li>
+                  <a class="logout" @click="logOut">Logout</a>
+                </li>
+              
+            </ul>
+        </nav>
+</header>
   <router-view/>
   <footer>
     <p>&copy; Cute Eve Limited  2022&nbsp;&nbsp;<a href="https://twitter.com/kabakiantony">Developed by</a></p>
