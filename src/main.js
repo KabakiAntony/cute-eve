8f6f80d993c1a805ccd3cddf89ca14ce190196bf
KabakiAntony <kabaki.antony@gmail.com>
2022-06-22 09:53:48 +0200

    reverted from environment variables

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ app.use(store)
 app.use(router);
 app.mount("#app");
 
-// app.config.globalProperties.$api = import.meta.env.VITE_API_URL
-// store.$api = import.meta.env.VITE_API_URL
-app.config.globalProperties.$api = import.meta.env.VITE_API_URL
-store.$api = import.meta.env.VITE_API_URL
+// app.config.globalProperties.$api = "http://127.0.0.1:5000/"
+// store.$api = "http://127.0.0.1:5000/"
+app.config.globalProperties.$api = "https://cute-eve-pos.herokuapp.com/"
+store.$api = "https://cute-eve-pos.herokuapp.com/"
 
 
